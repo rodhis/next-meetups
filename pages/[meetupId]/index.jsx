528bf891561d99aea5d24a5fe2c22b1ea9e86258
rodhis <rodrigonoliva@gmail.com>
2024-01-09 17:40:08 -0300

    set up Static Paths for meetup ids

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,4 +1,4 @@
-import { getStaticProps } from '..'
+
 import MeetupDetail from '../../components/meetups/MeetUpDetail'
 
 function MeetupDetails() {
@@ -13,21 +13,38 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            { params: {
+                meetupId: 'a1',
+            }},
+            { params: {
+                meetupId: 'a2',
+            }},
+            { params: {
+                meetupId: 'a3',
+            }},
+        ],
+    }
+}
+
 export async function getStaticProps(context) {
     //pega dados de um único meetup. como os dados não mudam muito, este é melhor que getServerSideProps.
 
-    const meetUpId = context.params.meetUpId
+    const meetupId = context.params.meetupId
 
     return {
         props: {
-            meetUpData: {
+            meetupData: {
                 image: "https://4.bp.blogspot.com/--ipiAvaSMQs/WNqtY1FcS8I/AAAAAAACFAs/Nd0cvmejnVUyMeeJlKjR8NzJyZu_M2o2QCLcB/s1600/13230110_1370193742995114_2334656076115372629_n.jpg",
-                id: meetUpId,
+                id: meetupId,
                 title: "Primeiro Rolê",
                 address: "Praça Matriz de Abaíra",
                 description: "Para todos se conhecerem!"
-            }
-        }
+            },
+        },
     }
 }
 
