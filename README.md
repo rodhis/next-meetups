fb16509ad47f76af9e063e9d9c7282d1158d019c
rodhis <rodrigonoliva@gmail.com>
2024-01-10 17:41:14 -0300

    finished APIs

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Simple app to schedule and organize your meetups with your friends!
 
-This is a Next.js practice project. It uses features such as dynamic pages names, Router, Next API building and a MongoDB Atlas integration!
+This is a Next.js practice project. It uses features such as dynamic pages names, data pre-rendering, Router, Next API building and a MongoDB Atlas integration!
 
 <img src='https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Next.js-900x0.png' alt='Next logo' width='200'>
--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,15 +1,15 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 require('dotenv').config()
 
 import MeetupDetail from '../../components/meetups/MeetUpDetail'
 
-function MeetupDetails() {
+function MeetupDetails(props) {
     return (
         <MeetupDetail
-            image= "https://4.bp.blogspot.com/--ipiAvaSMQs/WNqtY1FcS8I/AAAAAAACFAs/Nd0cvmejnVUyMeeJlKjR8NzJyZu_M2o2QCLcB/s1600/13230110_1370193742995114_2334656076115372629_n.jpg"
-            title= "Primeiro Rolê"
-            address= "Praça Matriz de Abaíra"
-            description= "Para todos se conhecerem!"
+            image={props.meetupData.image}
+            title= {props.meetupData.title}
+            address= {props.meetupData.address}
+            description= {props.meetupData.description}
         />
  
     )
@@ -26,6 +26,9 @@ export async function getStaticPaths() {
     const meetupsCollection = db.collection('meetups')
 
     const meetups = await meetupsCollection.find({}, {_id: 1}).toArray()
+
+    client.close()
+
     return {
         fallback: false,
         paths: meetups.map((meetup) => (
@@ -40,14 +43,27 @@ export async function getStaticProps(context) {
 
     const meetupId = context.params.meetupId
 
+    const username = process.env.USERNAME;
+    const password = process.env.PASSWORD
+
+    const client = await MongoClient.connect(`mongodb+srv://${username}:${password}@next1.vsk9a5j.mongodb.net/?retryWrites=true&w=majority`)
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups')
+
+    const selectedMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupId)
+        ,})
+
+    client.close()
+
     return {
         props: {
             meetupData: {
-                image: "https://4.bp.blogspot.com/--ipiAvaSMQs/WNqtY1FcS8I/AAAAAAACFAs/Nd0cvmejnVUyMeeJlKjR8NzJyZu_M2o2QCLcB/s1600/13230110_1370193742995114_2334656076115372629_n.jpg",
-                id: meetupId,
-                title: "Primeiro Rolê",
-                address: "Praça Matriz de Abaíra",
-                description: "Para todos se conhecerem!"
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                image: selectedMeetup.image,
+                description: selectedMeetup.description
             },
         },
     }
